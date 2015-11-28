cb6960e41a3b39d004e0fa745a9965ee47d9a04c
LetGo <a582o@163.com>
2015-11-28 12:59:49 +0800

    首次提交

---
+++ Assets/SkinMeshRenderHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinMeshRenderHolder : ScriptableObject
+{
+    public Object SMRGO;
+    public List<Material> Mats;
+    public List<string> Bones;
+}
