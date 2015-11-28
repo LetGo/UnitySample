b7ab6d7fb37a376d0228b0a5f258115a45db2078
LetGo <a582o@163.com>
2015-11-28 12:58:03 +0800

    首次提交

---
+++ Assets/Editor/AwatarAssetManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+public class AwatarAssetManager
+{
+    private static string RoleBaseAssetPath = Application.dataPath + "/Resources/Players/";
+    private static string MaterialPath = "Mats/";
+
+    public static bool GenerateMaterials(GameObject playermodel)
+    {
+        string matPath = RoleBaseAssetPath + MaterialPath;
+        matPath += playermodel.name + "/";
+
+        CheckCreateDir(matPath);
+
+        DeleteSpecFiles(matPath, ".mat");
+
+        string texturePath = GetModelRootPath(playermodel) + "/textures";
+
+        List<Texture2D> textureList = CollectAll<Texture2D>(texturePath);
+
+        var smrs = playermodel.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+
+        foreach (var smr in smrs)
+        {
+            foreach (var t in textureList)
+            {
+
+            }
+        }
+
+        return true;
+    }
+
+
+    static void CheckCreateDir(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    static void DeleteSpecFiles(string dirPath, string filterName)
+    {
+        var fileNames = Directory.GetFiles(dirPath);
+        fileNames = fileNames.Where(P => !P.EndsWith(".meta") && (string.IsNullOrEmpty(filterName) ? true : P.Contains(filterName))).ToArray();
+        
+        foreach (string fileName in fileNames)
+        {
+            File.Delete(fileName);
+        }
+    }
+
+    static string GetModelRootPath(GameObject model)
+    {
+        string path = AssetDatabase.GetAssetPath(model);
+        int index = path.LastIndexOf('/');
+        path = path.Substring(0, index);
+        return path;
+    }
+
+
+    public static List<T> CollectAll<T>(string path) where T : UnityEngine.Object
+    {
+        List<T> l = new List<T>();
+        string[] files = Directory.GetFiles(path);
+        for (int i = 0, imax = files.Length; i < imax; i++ )
+        {
+            if (files[i].EndsWith(".meta")) continue;
+            T asset = AssetDatabase.LoadAssetAtPath(files[i], typeof(T)) as T;
+            if (asset == null) throw new System.Exception("Asset is not "+ typeof(T) +":" + files[i]);
+            l.Add(asset);
+        }
+        return l;
+    }
+}
---
+++ Assets/Editor/AwatarEditor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AwatarEditor : EditorWindow
+{
+    static AwatarEditor m_editorWindow;
+    GameObject playerModel;
+    GameObject playerModelClone;
+
+    [MenuItem("Custom/Awatar %w")]
+    static void ShowWindow()
+    {
+        m_editorWindow = (AwatarEditor)GetWindow<AwatarEditor>();
+        m_editorWindow.Show();
+    }
+
+    bool CheckModelValid(GameObject model)
+    {
+        return !model.name.Contains("@");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("--- 选择模型拖放到下面 ---");
+        GUILayout.BeginHorizontal();
+        GameObject newModel = EditorGUILayout.ObjectField("FBX", playerModel,typeof(GameObject), true) as GameObject;
+
+        if (newModel != playerModel)
+        {
+            if (CheckModelValid(newModel))
+            {
+                playerModelClone = GameObject.Instantiate(newModel) as GameObject;
+
+                playerModel = newModel;
+            }
+            else
+            {
+                playerModel = null;
+                EditorUtility.DisplayDialog("选择模型错误", "不能选择动画模型.", "Ok");
+            }
+
+        }
+        GUILayout.EndHorizontal();
+
+        EditorGUILayout.Separator();
+
+        GUILayout.Label("--- 按照步奏进行拆解模型 ---");
+
+        if (GUILayout.Button("1-创建材质"))
+        {
+            if (playerModel != null)
+            {
+                if (AwatarAssetManager.GenerateMaterials(playerModel))
+                {
+                    EditorUtility.DisplayDialog("Character Generator", "成功创建材质.", "Ok");
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("Character Generator", "创建材质失败", "Ok");
+                }
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (playerModelClone != null)
+        {
+            GameObject.DestroyImmediate(playerModelClone);
+        }
+    }
+}
