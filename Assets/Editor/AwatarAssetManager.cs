37b2ad5c50e2b5aab2325fc2b7cf3cbe59d1c841
let_go <a582o@163.com>
2015-11-28 15:32:38 +0800

    分解动画，基础组件 材质

--- Assets/Editor/AwatarAssetManager.cs
+++ Assets/Editor/AwatarAssetManager.cs
@@ -7,12 +7,15 @@ using UnityEditor;
 
 public class AwatarAssetManager
 {
-    private static string RoleBaseAssetPath = Application.dataPath + "/Resources/Players/";
-    private static string MaterialPath = "Mats/";
+    private const string RoleBaseAssetPath ="Assets/Resources/Players/";
+    private const string MaterialPath = RoleBaseAssetPath  + "Mats/";
+    private const string AnimationPath = RoleBaseAssetPath + "Animations/";
+    private const string RoleBasePath = RoleBaseAssetPath + "RoleBase/";
+    private const string SkinPath = RoleBaseAssetPath + "Skins/";
 
     public static bool GenerateMaterials(GameObject playermodel)
     {
-        string matPath = RoleBaseAssetPath + MaterialPath;
+        string matPath = MaterialPath;
         matPath += playermodel.name + "/";
 
         CheckCreateDir(matPath);
@@ -27,15 +30,105 @@ public class AwatarAssetManager
 
         foreach (var smr in smrs)
         {
+            //TODO NORMAL TEXTURE  
+
             foreach (var t in textureList)
             {
+                if (t.name.ToLower().Contains("normal")) continue;
+                if (!t.name.ToLower().Contains(smr.name.ToLower())) continue;
 
+                string matSavePath = matPath + smr.name.ToLower() + ".mat";
+
+                Material m = new Material(Shader.Find(""));
+                m.SetColor("_COlor", new Color(0.6f, 0.6f, 0.6f));
+                m.SetTexture("_MainTex", t);
+
+                if (m != null)
+                {
+                    AssetDatabase.CreateAsset(m,matSavePath.Replace(Application.dataPath +"/",""));
+                }
             }
         }
 
         return true;
     }
 
+    public static bool SplitAnimations(GameObject playerModel) 
+    {
+        string animPath = AnimationPath + playerModel.name + "/";
+
+        CheckCreateDir(animPath);
+        DeleteSpecFiles(animPath,".anim");
+
+        List<GameObject> animaGOs = GetAllAnimGameObject( GetModelRootPath(playerModel), playerModel.name);
+
+        List<AnimationClip> animationClips = new List<AnimationClip>();
+        foreach (var item in animaGOs)
+        {
+            animationClips.AddRange(GetAnimationClipFromGO(item));
+        }
+
+        foreach (var item in animationClips)
+        {
+            var clipName = animPath + item.name.Replace("(Clone)","");
+            string clipPath = clipName + ".anim";
+
+            AssetDatabase.CreateAsset(item, clipPath);
+        }
+        return true;
+    }
+
+    public static bool GenerateRoleBaseAsset(GameObject plaerModel)
+    {
+        string basePath = RoleBasePath;
+        CheckCreateDir(basePath);
+        DeleteSpecFiles(basePath, plaerModel.name);
+
+        var modelClone = GameObject.Instantiate(plaerModel) as GameObject;
+
+        if (modelClone != null)
+        {
+            var anim = modelClone.GetComponent<Animation>();
+            if(anim != null) Object.DestroyImmediate(anim);
+            var animtor = modelClone.GetComponent<Animator>();
+            if (animtor != null) Object.DestroyImmediate(animtor);
+
+            var mes = modelClone.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (var item in mes)
+            {
+                Object.DestroyImmediate(item);
+            }
+            CreatePrefab(modelClone, basePath + plaerModel.name + ".prefab");
+        }
+        return true;
+    }
+
+    static Object CreatePrefab(GameObject go, string path) 
+    {
+        Object prefab = PrefabUtility.CreatePrefab(path,go);
+        Object.DestroyImmediate(go);
+        return prefab;
+    }
+
+    static List<AnimationClip> GetAnimationClipFromGO(GameObject model) 
+    {
+        List<AnimationClip> clips = new List<AnimationClip>();
+
+        GameObject go = GameObject.Instantiate(model) as GameObject;
+        if (go != null)
+        {
+            var getClips = AnimationUtility.GetAnimationClips(go);
+
+            foreach (var item in getClips)
+            {
+                var tempClip = Object.Instantiate(item) as AnimationClip;
+                tempClip.name = item.name;
+                clips.Add(tempClip);
+            }
+            GameObject.DestroyImmediate(go);
+        }
+        return clips;
+    }
 
     static void CheckCreateDir(string path)
     {
@@ -64,6 +157,12 @@ public class AwatarAssetManager
         return path;
     }
 
+    private static List<GameObject> GetAllAnimGameObject(string dirPath,string modelName) 
+    {
+        List<GameObject> l = CollectAll<GameObject>(dirPath);
+
+        return l.FindAll(C => C.name.StartsWith(modelName +"@"));
+    }
 
     public static List<T> CollectAll<T>(string path) where T : UnityEngine.Object
     {
