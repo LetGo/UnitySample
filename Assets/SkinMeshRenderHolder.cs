cb13048e823c8139e3111b54c05b4465db9f8660
chengxue.zhao <369800228@qq.com>
2017-05-17 21:30:16 +0800

    delete

--- Assets/SkinMeshRenderHolder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class SkinMeshRenderHolder : ScriptableObject
-{
-    public List<Material> Mats;
-    public List<string> Bones;
-    public Mesh sharedMesh;
-}
